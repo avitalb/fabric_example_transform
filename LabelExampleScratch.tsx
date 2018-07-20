c9187130f3fe4788aeb203b85ba9b4abc46bd4c4
Avital Baral <t-avba@microsoft.com>
2018-07-20 10:19:45 -0700

    init commit

---
+++ LabelExampleScratch.tsx
@@ -0,0 +1,11 @@
+// import * as React from 'react';
+
+import { Label } from "c:/Users/t-avba/src/office-ui-fabric-react/packages/office-ui-fabric-react/src/Label";
+
+export const LabelBasicExample = () => (
+  <div>
+    <Label>I'm a Label</Label>
+    <Label disabled={true}>I'm a disabled Label</Label>
+    <Label required={true}>I'm a required Label</Label>
+  </div>
+);
---
+++ transform.js
@@ -0,0 +1,46 @@
+const j = require('jscodeshift');
+const babylon = require('babylon');
+const recast = require('recast');
+
+function parseRaw(code) {
+  return recast.parse(code, { parser: { parse } }).program.body;
+}
+
+function transform(file, api) {
+  const parse = source =>
+    babylon.parse(source, {
+      sourceType: 'module',
+      plugins: ['jsx', 'typescript']
+    });
+
+  const j = api.jscodeshift;
+
+  // delete export declaration (use literal and not exportDeclaration to only delete the actual "export" word)
+
+  let source =   j(recast.parse(file.source, { parser: { parse } })).find(j.literal('export')).remove().toSource();
+  // // attach all imported components to the window
+  // const root = j(file.source);
+  // const imports = root.find(j.importDeclaration);
+
+  // let identifiers = [];
+  // imports.forEach(p => {
+  //   identifiers += p.node.identifier;
+  // });
+
+  // let source = j(recast.parse(file.source, { parser: { parse } })).insertAfter(j.literal('let {'));
+
+  // for (var i = 0; i < identifiers.length; i++) {
+  //   source.insertAfter(j.literal(identifiers[i] + ','));
+  // }
+  // source.insertAfter(j.literal(' Fabric } = window.Fabric;')).toSource();
+
+  // //find name of example
+  // const exampleName = root.find(j.exportDeclaration.name);
+
+
+  //add footer
+  // j.literal("ReactDOM.render(<"+exampleName+"/>,document.getElementById('content'));"
+  return source;
+}
+
+module.exports = transform;
