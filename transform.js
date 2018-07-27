182647de70822b05d5db8467c642874aea3858ea
Avital Baral <t-avba@microsoft.com>
2018-07-27 09:23:51 -0700

    support transform of class declarations

--- transform.js
+++ transform.js
@@ -14,7 +14,7 @@ function transform(file, api) {
   const parse = source =>
     babylon.parse(source, {
       sourceType: 'module',
-      plugins: ['jsx', 'typescript']
+      plugins: ['jsx', 'typescript','classProperties']
     });
 
   const j = api.jscodeshift;
@@ -25,10 +25,10 @@ function transform(file, api) {
   source = source.find(j.ImportDeclaration,node => node.source.value == 'react').remove().toSource();
   source = j(recast.parse(source, { parser: { parse } }));
 
-  //remove css import
+  //remove css imports
   source = source.find(j.ImportDeclaration,node => node.source.value.endsWith('css')).remove().toSource();
   source = j(recast.parse(source, { parser: { parse } }));
-  
+
   // attach all imported components to the window
   let attachedWindowString = "let {\n";
 
@@ -42,9 +42,9 @@ function transform(file, api) {
 
 
   for (var i = 0; i < identifiers.length; i++) {
-    attachedWindowString += identifiers[i] + ',\n';
+    attachedWindowString += '\t' + identifiers[i] + ',\n';
   }
-  attachedWindowString += 'Fabric } = window.Fabric;';
+  attachedWindowString += '\t Fabric } = window.Fabric;';
 
 
   let parsedAttachedWindowString = parseRaw(attachedWindowString);
@@ -53,20 +53,36 @@ function transform(file, api) {
   source = source.find(j.ImportDeclaration).remove().toSource();
   source = j(recast.parse(source, { parser: { parse } }));
 
-  // remove exports and replace with variable declarations
-  let exportDeclarations = source.find(j.ExportNamedDeclaration);
+  // remove exports and replace with variable or class declarations, whichever the original example used
+  let variableExportDeclarations = source.find(j.ExportNamedDeclaration, node => node.declaration.type == "VariableDeclaration"); 
+  let classExportDeclarations = source.find(j.ExportNamedDeclaration, node => node.declaration.type == "ClassDeclaration");
   let exampleName;
 
-  // there should only be 1 export declaration but forEach is the only way of actually interacting with the tree nodes
-  exportDeclarations.forEach(
-    p=> exampleName = p.node.declaration.declarations[0].id.name
-  )
-  exportDeclarations.forEach(
-    p => source = source.find(j.ExportNamedDeclaration).replaceWith(p.node.declaration).insertBefore(parsedAttachedWindowString))
+  // for examples with variable export declarations
+  if (variableExportDeclarations.__paths.length > 0) {
+    variableExportDeclarations.forEach(
+      p=> exampleName = p.node.declaration.declarations[0].id.name
+    )
+
+    variableExportDeclarations.forEach(
+      p => source = source.find(j.ExportNamedDeclaration).replaceWith(p.node.declaration).insertBefore(parsedAttachedWindowString))
+  }
+
+  // for examples which export react components as a class
+  else if (classExportDeclarations.paths.length > 0) {
+    // extract name
+    classExportDeclarations.forEach(
+      p => exampleName = p.node.declaration.id.name
+    )
+    
+    // extract class declaration
+    classExportDeclarations.forEach(
+      p => source = source.find(j.ExportNamedDeclaration).replaceWith(p.node.declaration).insertBefore(parsedAttachedWindowString)
+    )
+  }
 
   // add React Render footer
   const sourceWithFooter = source.toSource() + "\n" + 'ReactDOM.render(<'+ exampleName + '/>,document.getElementById("content"));';
-
   return sourceWithFooter;
 }
 
